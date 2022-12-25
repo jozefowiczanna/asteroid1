e5c7192273455c0681c5648f9494a38b986729c4
Anna Jozefowicz <anna.jozefowicz.86@gmail.com>
2022-12-25 10:33:55 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame, sys
+
+WINDOW_WIDTH, WINDOW_HEIGHT = 800, 600
+pygame.init()
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Asteroids")
+
+# create a surface
+test_surf = pygame.Surface((400,100))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    display_surface.fill((20,120,250))
+    test_surf.fill((0,200,20))
+    display_surface.blit(test_surf, (0,0))
+    pygame.display.update()
