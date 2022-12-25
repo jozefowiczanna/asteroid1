360d143b5af2b39b01aff94a248e95ba32cd599a
Anna Jozefowicz <anna.jozefowicz.86@gmail.com>
2022-12-25 17:02:01 +0100

    update main.py

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ WINDOW_WIDTH, WINDOW_HEIGHT = 800, 600
 pygame.init()
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Asteroids")
+clock = pygame.time.Clock()
 
+ship_y_pos = 500
 bg_surf = pygame.image.load('asteroid/graphics/background.png').convert()
 ship_surf = pygame.image.load('asteroid/graphics/ship.png').convert_alpha()
 
@@ -12,15 +14,20 @@ font = pygame.font.Font('asteroid/graphics/subatomic.ttf', 50)
 text_surf = font.render('Space', True, (255,255,255))
 
 while True:
+
+    # 1. input -> events (mouse click, mouse movement, press of a button, controller or touchscreen)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
+    # framerate limit
+    clock.tick(120)
+
+    # 2. updates
     display_surface.fill((100,100,100))
     display_surface.blit(bg_surf, (0,0))
-    display_surface.blit(ship_surf, (100,50))
+    ship_y_pos -= 4
+    display_surface.blit(ship_surf, (100,ship_y_pos))
     display_surface.blit(text_surf, (400,300))
     pygame.display.update()
-
-    
