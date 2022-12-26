ab8521a40efbe3d8db54b76c82e24226eeccc2ad
Anna Jozefowicz <anna.jozefowicz.86@gmail.com>
2022-12-26 20:13:21 +0100

    update main.py

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 import pygame, sys
 
-WINDOW_WIDTH, WINDOW_HEIGHT = 800, 600
+# game init
 pygame.init()
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Asteroids")
 clock = pygame.time.Clock()
 
+# background
 bg_surf = pygame.image.load('asteroid/asteroid1/graphics/background.png').convert()
+
+# ship import
 ship_surf = pygame.image.load('asteroid/asteroid1/graphics/ship.png').convert_alpha()
 ship_rect = ship_surf.get_rect(center = (WINDOW_WIDTH/2,WINDOW_HEIGHT/2))
 
+# text import
 font = pygame.font.Font('asteroid/asteroid1/graphics/subatomic.ttf', 50)
 text_surf = font.render('Space', True, (255,255,255))
 text_rect = text_surf.get_rect(midbottom = (WINDOW_WIDTH/2, WINDOW_HEIGHT))
 
-while True:
+# laser import
+laser_surf = pygame.image.load('asteroid/asteroid1/graphics/laser.png').convert_alpha()
+laser_rect = laser_surf.get_rect(midbottom = ship_rect.midtop)
 
-    # 1. input -> events (mouse click, mouse movement, press of a button, controller or touchscreen)
+while True:
+    # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -26,12 +34,17 @@ while True:
     clock.tick(120)
 
     # mouse input
-    # place the ship on the mouse position while using the methods
     ship_rect.center = pygame.mouse.get_pos()
-    # 2. updates
+
+    # update
+    laser_rect.y -= 4
+
+    # drawing
     display_surface.fill((100,100,100))
     display_surface.blit(bg_surf, (0,0))
-    # if the top of the ship is at the top of the window -> stop the movement
     display_surface.blit(ship_surf, ship_rect)
     display_surface.blit(text_surf, text_rect)
+    display_surface.blit(laser_surf, laser_rect)
+
+    # draw the final frame
     pygame.display.update()
